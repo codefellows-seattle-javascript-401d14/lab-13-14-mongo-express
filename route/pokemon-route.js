c9fd240df36b58d096defa907e028efa67c7e04c
devonhackley <devonhackley@gmail.com>
2017-01-19 15:46:45 -0800

    have all test passing, need to work on readme

--- route/pokemon-route.js
+++ route/pokemon-route.js
@@ -3,9 +3,10 @@
 const Router = require('express').Router;
 const debug = require('debug')('pokeapp:pokemon-route');
 const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
 
 const Pokemon = require('../model/pokemon.js');
-const Trainer = require('../model/trainer.js');
+// const Trainer = require('../model/trainer.js');
 
 const pokemonRouter = module.exports = new Router();
 
@@ -16,4 +17,18 @@ pokemonRouter.post('/api/pokemons', jsonParser, function(req,res,next){
     res.json(pokemon);
   })
   .catch(next);
+});
+
+pokemonRouter.get('/api/pokemons/:id', function(req,res,next){
+  debug('GET /api/pokemons/:id');
+  Pokemon.findById(req.params.id)
+  .then(pokemon => res.json(pokemon))
+  .catch(err => next(createError(404,err.message)));
+});
+
+pokemonRouter.delete('/api/pokemons/:id', function(req,res,next){
+  debug('Delete /api/pokemons/:id');
+  Pokemon.findByIdAndRemove(req.params.id)
+  .then(() => res.status(204).send())
+  .catch(err => next(createError(404,err.message)));
 });
--- test/pokemon-route-test.js
+++ test/pokemon-route-test.js
@@ -100,8 +100,84 @@ describe('Testing pokemon router', function(){
       }).save()
       .then(trainer => {
         this.tempTrainer = trainer;
-        
+        return new Pokemon({
+          name: 'Blastoise',
+          type: 'water',
+          pokedexNum: 9,
+          trainerID: this.tempTrainer._id.toString(),
+        }).save();
       })
-    })
+      .then(pokemon => {
+        this.tempPokemon = pokemon;
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should respond with a pokemon and status code of 200', (done) => {
+      superagent.get(`${apiURL}/api/pokemons/${this.tempPokemon._id.toString()}`)
+      .then(res => {
+        expect(res.status).to.equal(200);
+        expect(res.body.trainerID).to.equal(this.tempTrainer._id.toString());
+        expect(res.body.name).to.equal('Blastoise');
+        expect(res.body.type).to.equal('water');
+        expect(res.body.pokedexNum).to.equal(9);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should respond with a status code of 404', (done) => {
+      superagent.get(`${apiURL}/api/pokemons/12`)
+      .then(done)
+      .catch((err) => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
+
+  });
+
+  describe('Testing DELETE /api/pokemons:id', function(){
+    beforeEach((done) => {
+      new Trainer({
+        name: 'Gary',
+        age: '13',
+      }).save()
+      .then(trainer => {
+        this.tempTrainer = trainer;
+        return new Pokemon({
+          name: 'Blastoise',
+          type: 'water',
+          pokedexNum: 9,
+          trainerID: this.tempTrainer._id.toString(),
+        }).save();
+      })
+      .then(pokemon => {
+        this.tempPokemon = pokemon;
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should delete a pokemon and respond with a status of 204', (done) => {
+      superagent.delete(`${apiURL}/api/pokemons/${this.tempPokemon._id.toString()}`)
+      .then(res => {
+        expect(res.status).to.equal(204);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('should respond with a 404 status code', (done) => {
+      superagent.delete(`${apiURL}/api/pokemons/23232`)
+      .then(done)
+      .catch((err) => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
   });
 });
