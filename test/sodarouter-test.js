c853e0aa823597d7c95ad71bb53c581731448c33
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-01-22 23:03:12 -0800

    testing writing isn't the issue, it's adding all the other modules in server.js

--- test/sodarouter-test.js
+++ test/sodarouter-test.js
@@ -15,6 +15,7 @@ describe('should return a soda with a valid ID', function(){
     .catch(done);
   });
   describe('testing /api/soda', function(){
+
     it('should return a soda with valid body',(done) => {
       superagent.post(`${baseURL}/api/soda`)
       .send({
@@ -26,13 +27,27 @@ describe('should return a soda with a valid ID', function(){
     .then(res => {
       expect(res.status).to.equal(200);
       expect(res.body.brand).to.equal('RC');
+      expect(res.body.calories).to.equal(230);
       expect(res.body.diet).to.equal(false);
       expect(res.body.taste).to.equal('too sweet');
       done();
     })
     .catch(done);
     }); //end of it block
+    it('should return a 400 status',(done) => {
+      superagent.post(`${baseURL}/api/soda`)
+      .send({
+        // brand: 'RC',
+        // calories: 230,
+        // diet: true,
+        // taste: 'too sweet',
+      })
+    .then(done)
+    .catch(err => {
+      expect(err.status).to.equal(400);
+      done();
+    })
+    .catch(done);
+    }); //end of it block
   });
-
-
 });
